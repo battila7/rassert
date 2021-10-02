f446b18b51491df277d7f0b214963cd1a4c61811
Attila Bagossy <bagossyattila@outlook.com>
2021-10-02 13:05:41 +0200

    fix: removed the intentionally failing test case

--- src/lib.rs
+++ src/lib.rs
@@ -9,15 +9,3 @@ pub mod prelude {
 
     pub use super::expectations::*;
 }
-
-#[cfg(test)]
-mod tests {
-    use super::prelude::*;
-
-    #[test]
-    fn it_works() {
-        let a: Result<u32, u64> = Err(10);
-
-        expect_matches!(&a, Ok(c) if c > &10).conclude_panic();
-    }
-}
